f5e00f35e1559649e9049dcf8e8f6fc214442503
Jake Richard <me@jakerichard.tech>
2025-09-19 18:59:42 -0500

    Added favicon, better page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Jake Richard",
+  title: "Jake Richard | Portfolio",
   description: "Portfolio, sandbox, and general-purpose echo chamber",
 };
 
