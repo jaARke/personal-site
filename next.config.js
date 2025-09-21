bcdbb5bdb5659c81044ae7fbab91e8428858aae3
Jake Richard <me@jakerichard.tech>
2025-09-21 01:03:03 -0500

    Test deployment with next.config.js updates

--- next.config.js
+++ next.config.js
@@ -1,30 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   rewrites: async () => {
-    return [
-      {
+    const rewrites = [];
+    
+    if (process.env.NODE_ENV === "development") {
+      rewrites.push({
         source: "/api/py/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/:path*"
-            : "/api/",
-      },
-      {
-        source: "/docs",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/docs"
-            : "/api/py/docs",
-      },
-      {
-        source: "/openapi.json",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/openapi.json"
-            : "/api/py/openapi.json",
-      },
-    ];
-  },
+        destination: "http://127.0.0.1:8000/api/py/:path*"
+      });
+    }
+    
+    return rewrites;
+  }
 };
 
 module.exports = nextConfig;
